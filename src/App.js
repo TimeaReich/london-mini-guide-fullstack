dd4d4e12e6a16f6955453ad4a5069ec757056945
Timea <timea.reich@gmail.com>
2022-05-31 19:31:08 +0100

    test to fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dropdown from "./Dropdown";
+import CategoriesButtons from "./CategoriesButtons";
+import SearchResult from "./SearchResult";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [selectedCity, setSelectedCity] = useState("");
+  const [category, setCategory] = useState("");
+  const [data, setData] = useState("");
+
+  console.log(selectedCity);
+  console.log(category);
+
+  useEffect(() => {
+    fetch(`${selectedCity}/${category}`)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log(err));
+  }, [category, selectedCity]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>London Mini Guide</h1>
+      <Dropdown setSelectedCity={setSelectedCity} />
+      <CategoriesButtons category={category} setCategory={setCategory} />
+      <SearchResult data={data} />
     </div>
   );
 }
